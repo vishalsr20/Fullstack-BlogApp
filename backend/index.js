e2e7943cd61f124baeefcb21e48f0e04bfa8965f
Vishal Rathod <vishalrathod191918@gmail.com>
2025-06-17 21:49:46 +0530

    fix issue in the index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.json())
 app.use(cookieParser())
-
+// app.use(cors())
 
 app.use(cors({
   origin: "https://techthinkersblog.onrender.com",
