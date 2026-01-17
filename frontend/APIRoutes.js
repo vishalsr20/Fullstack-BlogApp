e755618ecd6e043e90f5ce731c85067df93758ed
Vishal Rathod <vishalrathod191918@gmail.com>
2026-01-17 14:01:36 +0530

    (fix): api routes

--- frontend/APIRoutes.js
+++ frontend/APIRoutes.js
@@ -1,4 +1,4 @@
-//  const host = 'https://techthinkers.onrender.com'
+  //const host = 'https://techthinkers.onrender.com'
  const host ='http://localhost:3000'
 
 export const LoginRoutes = `${host}/api/v1/login`
